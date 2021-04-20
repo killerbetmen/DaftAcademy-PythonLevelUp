ff4095489b66218ca7ae075bb3b8504089c83eec
Misha Lezhnin <mikellezhnin20@gmail.com>
2021-04-20 02:38:00 +0200

    add methods

--- main.py
+++ main.py
@@ -1,8 +1,51 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 app = FastAPI()
+app.counter = 0
+
+
+class HelloResp(BaseModel):
+    msg: str
 
 
 @app.get('/')
 def root():
-    return{'message': "Hello world"}
+    return{'message': "Hello world!"}
+
+
+@app.get('/hello/{name}', response_model=HelloResp)
+def hello_name_view(name: str):
+    return HelloResp(msg=f"Hello {name}")
+
+
+@app.get('/counter')
+def counter():
+    app.counter += 1
+    return str(app.counter)
+
+
+@app.get("/method", status_code=200)
+def give_method():
+    return {"method": "GET"}
+
+
+@app.put("/method", status_code=200)
+def give_method():
+    return {"method": "PUT"}
+
+
+@app.options("/method", status_code=200)
+def give_method():
+    return {"method": "OPTIONS"}
+
+
+@app.delete("/method", status_code=200)
+def give_method():
+    return {"method": "DELETE"}
+
+
+@app.post("/method", status_code=201)
+def give_method():
+    return {"method": "POST"}
--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
 from main import app
+import pytest
 
 client = TestClient(app)
 
@@ -7,4 +8,43 @@ client = TestClient(app)
 def test_read_main():
     response = client.get('/')
     assert response.status_code == 200
-    assert response.json == {'message': 'Hello World'}
+    assert response.json == {'message': 'Hello World!'}
+
+
+@pytest.mark.parametrize('name', ['Zenek', 'Marek', 'Alojzy Niezdąży'])
+def test_hello_name(name):
+    response = client.get(f'/hello/{name}')
+    assert response.status_code == 200
+    assert response.text == f'"Hello {name}"'
+
+
+def test_counter(name):
+    response = client.get(f"/counter")
+    assert response.status_code == 200
+    assert response.text == "1"
+    # 2nd Try
+    response = client.get(f"/counter")
+    assert response.status_code == 200
+    assert response.text == "2"
+
+
+def test_methods():
+    response = client.get(f"/method")
+    assert response.status_code == 200
+    assert response.json() == {"method": "GET"}
+
+    response = client.post(f"/method")
+    assert response.status_code == 201
+    assert response.json() == {"method": "POST"}
+
+    response = client.put(f"/method")
+    assert response.status_code == 200
+    assert response.json() == {"method": "PUT"}
+
+    response = client.options(f"/method")
+    assert response.status_code == 200
+    assert response.json() == {"method": "OPTIONS"}
+
+    response = client.delete(f"/method")
+    assert response.status_code == 200
+    assert response.json() == {"method": "DELETE"}
