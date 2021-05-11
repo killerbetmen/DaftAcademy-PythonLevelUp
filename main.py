10a5aa24d211b7799299b36fa65db29914882a3d
Misha Lezhnin <mikellezhnin20@gmail.com>
2021-05-11 23:33:38 +0200

    add homework 4.6

--- main.py
+++ main.py
@@ -275,31 +275,33 @@ async def product(id: int):
         }
 
 
+def view_order(order):
+    if order not in {"first_name", "last_name", "city", "EmployeeID"}:
+        raise HTTPException(status_code=400)
+
+
 @app.get("/employees")
 async def get_employees(limit: Optional[int] = Query(None), offset: Optional[int] = Query(None),
-                        order: Optional[str] = Query(None)):
-    order_accepted = ['last_name', 'first_name', 'city']
-    if order is not None and order not in order_accepted:
-        raise HTTPException(status_code=400)
-    else:
-        app.db_connection.row_factory = sqlite3.Row
-        query = f"SELECT EmployeeID, LastName, FirstName, City FROM Employees ORDER BY {order} ASC"
-        if limit is not None:
-            query += f" LIMIT {limit}"
-        if offset is not None:
-            query += f" OFFSET {offset}"
-        data = app.db_connection.execute(query).fetchall()
-        return {
-            "employees": [
-                {
-                    "id": x["EmployeeID"],
-                    "last_name": x["LastName"],
-                    "first_name": x["FirstName"],
-                    "city": x["City"],
-                }
-                for x in data
-            ]
-        }
+                        order: Optional[str] = Query('EmployeeID')):
+    view_order(order)
+    app.db_connection.row_factory = sqlite3.Row
+    query = f"SELECT EmployeeID, LastName, FirstName, City FROM Employees ORDER BY {order} ASC"
+    if limit is not None:
+        query += f" LIMIT {limit}"
+    if offset is not None:
+        query += f" OFFSET {offset}"
+    data = app.db_connection.execute(query).fetchall()
+    return {
+        "employees": [
+            {
+                "id": x["EmployeeID"],
+                "last_name": x["LastName"],
+                "first_name": x["FirstName"],
+                "city": x["City"],
+            }
+            for x in data
+        ]
+    }
 
 
 @app.get('/products_extended')
