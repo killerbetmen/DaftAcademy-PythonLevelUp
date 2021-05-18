bf84b04d9888cb7bd2104a7bc9010d4c972f5277
Misha Lezhnin <mikellezhnin20@gmail.com>
2021-05-19 01:26:15 +0200

    update database url

--- app/database.py
+++ app/database.py
@@ -1,11 +1,11 @@
 import os
+import psycopg2
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+DATABASE_URL = os.environ["DATABASE_URL"]
 
-SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = psycopg2.connect(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
