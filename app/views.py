d3a9458e6cac9efde02aed06fd7c9fb667ad3de7
Misha Lezhnin <mikellezhnin20@gmail.com>
2021-05-19 09:07:18 +0200

    add homework 5.1

--- app/views.py
+++ app/views.py
@@ -3,21 +3,33 @@ from typing import List
 from fastapi import APIRouter, Depends, HTTPException
 from pydantic import PositiveInt
 from sqlalchemy.orm import Session
-
-from . import crud, schemas
+from . import crud, schemas, models
 from .database import get_db
 
 router = APIRouter()
 
 
-@router.get("/shippers/{shipper_id}", response_model=schemas.Shipper)
+@router.get("/shippers/{shipper_id}")
 async def get_shipper(shipper_id: PositiveInt, db: Session = Depends(get_db)):
-    db_shipper = crud.get_shipper(db, shipper_id)
+    db_shipper = get_shipper(db, shipper_id)
     if db_shipper is None:
         raise HTTPException(status_code=404, detail="Shipper not found")
     return db_shipper
 
 
-@router.get("/shippers", response_model=List[schemas.Shipper])
+@router.get("/shippers")
 async def get_shippers(db: Session = Depends(get_db)):
-    return crud.get_shippers(db)
+    return get_shippers(db)
+
+
+@router.get("/suppliers/{supplier_id}")
+async def get_supplier(supplier_id: PositiveInt, db: Session = Depends(get_db)):
+    db_supplier = get_supplier(db, supplier_id)
+    if db_supplier is None:
+        raise HTTPException(status_code=404, detail="Supplier not found")
+    return db_supplier
+
+
+@router.get("/suppliers")
+async def get_suppliers(db: Session = Depends(get_db)):
+    return get_suppliers(db)
