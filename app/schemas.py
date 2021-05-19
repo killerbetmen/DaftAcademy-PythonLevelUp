b5182f76a6d049311576230334bca4e412737f29
Misha Lezhnin <mikellezhnin20@gmail.com>
2021-05-19 09:15:14 +0200

    add homework 5.2

--- app/schemas.py
+++ app/schemas.py
@@ -1,41 +1,7 @@
 from pydantic import BaseModel, PositiveInt, constr
 from typing import Optional
+from typing_extensions import TypedDict
 
-
-class HelloResp(BaseModel):
-    msg: str
-
-
-class Message(BaseModel):
-    msg: str
-
-
-class Person(BaseModel):
-    name: str
-    surname: str
-
-
-class RegisteredUser(BaseModel):
-    id: int
-    name: str
-    surname: str
-    register_date: str
-    vaccination_date: str
-
-
-class PatientIdentifier(BaseModel):
-    id: int
-
-
-class Token(BaseModel):
-    token: str
-
-
-class Category(BaseModel):
-    name: str
-
-
-# ORM MODELS
 
 class Shipper(BaseModel):
     ShipperID: PositiveInt
@@ -49,12 +15,30 @@ class Shipper(BaseModel):
 class Supplier(BaseModel):
     SupplierID: PositiveInt
     CompanyName: constr(max_length=40)
+    ContactName: constr(max_length=30)
+    ContactTitle: constr(max_length=30)
+    Address: constr(max_length=60)
+    City: constr(max_length=15)
+    Region: Optional[constr(max_length=15)]
+    PostalCode: constr(max_length=10)
+    Country: constr(max_length=15)
+    Phone: constr(max_length=24)
+    Fax: Optional[constr(max_length=24)] = None
+    HomePage: Optional[constr(max_length=255)] = None
 
     class Config:
         orm_mode = True
 
 
-class CategoryData(BaseModel):
+class Suppliers(BaseModel):
+    SupplierID: PositiveInt
+    CompanyName: constr(max_length=40)
+
+    class Config:
+        orm_mode = True
+
+
+class Category(BaseModel):
     CategoryID: PositiveInt
     CategoryName: str
 
@@ -62,56 +46,11 @@ class CategoryData(BaseModel):
         orm_mode = True
 
 
-class SupplierProduct(BaseModel):
-    ProductID: PositiveInt
-    ProductName: str
-    Category: CategoryData
-    Discontinued: int
-
-    class Config:
-        orm_mode = True
-
-
-class PostedSupplier(BaseModel):
-    CompanyName: str
-    ContactName: Optional[str]
-    ContactTitle: Optional[str]
-    Address: Optional[str]
-    City: Optional[str]
-    PostalCode: Optional[str]
-    Country: Optional[str]
-    Phone: Optional[str]
-
-    class Config:
-        orm_mode = True
-
-
-class ReturnedSupplier(BaseModel):
-    SupplierID: PositiveInt
-    CompanyName: Optional[str]
-    ContactName: Optional[str]
-    ContactTitle: Optional[str]
-    Address: Optional[str]
-    City: Optional[str]
-    PostalCode: Optional[str]
-    Country: Optional[str]
-    Phone: Optional[str]
-    Fax: Optional[str]
-    HomePage: Optional[str]
-
-    class Config:
-        orm_mode = True
-
-
-class UpdatedSupplier(BaseModel):
-    CompanyName: Optional[str]
-    ContactName: Optional[str]
-    ContactTitle: Optional[str]
-    Address: Optional[str]
-    City: Optional[str]
-    PostalCode: Optional[str]
-    Country: Optional[str]
-    Phone: Optional[str]
+class Products(BaseModel):
+    ProductID: Optional[PositiveInt]
+    ProductName: Optional[constr(max_length=40)]
+    Category: Optional[Category]
+    Discontinued: Optional[int]
 
     class Config:
         orm_mode = True
--- app/views.py
+++ app/views.py
@@ -3,33 +3,41 @@ from typing import List
 from fastapi import APIRouter, Depends, HTTPException
 from pydantic import PositiveInt
 from sqlalchemy.orm import Session
-from . import crud, schemas, models
+from . import crud, schemas
 from .database import get_db
 
 router = APIRouter()
 
 
-@router.get("/shippers/{shipper_id}")
+@router.get("/shippers/{shipper_id}", response_model=schemas.Shipper)
 async def get_shipper(shipper_id: PositiveInt, db: Session = Depends(get_db)):
-    db_shipper = get_shipper(db, shipper_id)
+    db_shipper = crud.get_shipper(db, shipper_id)
     if db_shipper is None:
         raise HTTPException(status_code=404, detail="Shipper not found")
     return db_shipper
 
 
-@router.get("/shippers")
+@router.get("/shippers", response_model=List[schemas.Shipper])
 async def get_shippers(db: Session = Depends(get_db)):
-    return get_shippers(db)
+    return crud.get_shippers(db)
 
 
-@router.get("/suppliers/{supplier_id}")
+@router.get("/suppliers", response_model=List[schemas.Suppliers])
+async def get_suppliers(db: Session = Depends(get_db)):
+    return crud.get_suppliers(db)
+
+
+@router.get("/suppliers/{supplier_id}", response_model=schemas.Supplier)
 async def get_supplier(supplier_id: PositiveInt, db: Session = Depends(get_db)):
-    db_supplier = get_supplier(db, supplier_id)
+    db_supplier = crud.get_supplier(db, supplier_id)
     if db_supplier is None:
         raise HTTPException(status_code=404, detail="Supplier not found")
     return db_supplier
 
 
-@router.get("/suppliers")
-async def get_suppliers(db: Session = Depends(get_db)):
-    return get_suppliers(db)
+@router.get("/suppliers/{supplier_id}/products", response_model=List[schemas.Products])
+async def get_product(supplier_id: PositiveInt, db: Session = Depends(get_db)):
+    db_products = crud.get_product(db, supplier_id)
+    if not db_products:
+        raise HTTPException(status_code=404, detail="Supplier not found")
+    return db_products
